fdad5aaeacb720a40a7657478ae22ad63ce1f0da
sunita persad <sunitapersad020@gmail.com>
2021-10-03 15:32:45 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var cityInput = document.querySelector('#city-input');
+var cityBtn = document.querySelector('#search-btn');
+var cityNameEl = document.querySelector('#city-name');
+var cityArr = [];
+var apiKey = 'e4ab7318fab329c7de8c4fd9dd5056d7'; // please enter API Key here
+
